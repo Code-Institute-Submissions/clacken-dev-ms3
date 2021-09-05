e1678f2e5b0cb4f3d3984edf6a65d9a0490d4c4b
clacken-dev <lacken596@gmail.com>
2021-09-05 10:17:39 +0000

    Style overview.html:

--- static/js/script.js
+++ static/js/script.js
@@ -15,8 +15,13 @@ let ward_c_percent = (ward_c_patients/ward_c_max_patients);
 document.getElementById("ward-c-percent").innerHTML = Math.trunc((ward_c_percent*100)) + "% Full";
 //Get percentage capacity of Ward D
 let ward_d_patients = document.getElementById('current-ward-d').innerHTML;
-console.log(ward_d_patients);
 let ward_d_max_patients = 20;
 let ward_d_percent = (ward_d_patients/ward_d_max_patients);
-console.log(ward_d_percent);
 document.getElementById("ward-d-percent").innerHTML = Math.trunc((ward_d_percent)*100) + "% Full";
+//Total Critical
+let total_critical = document.getElementById('total-critical').innerHTML;
+document.getElementById('critical-condition').innerHTML = total_critical;
+//Percentage of total patients
+let total_patients = document.getElementById('total-patients').innerHTML;
+let total_percent = (total_patients/70);
+document.getElementById('percentage-of-total').innerHTML = Math.trunc(total_patients) + "% Full";
